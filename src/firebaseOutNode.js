8375bf6df6128e5b6ddfcaa7ba962cda4f14da9d
Nam Giang <kyng@ece.ubc.ca>
2020-03-04 17:42:14 -0800

    firebase cloud messaging begin

--- src/firebaseOutNode.js
+++ src/firebaseOutNode.js
@@ -3,54 +3,12 @@ function FirebaseOutNode(config) {
     throw "No firebase admin specified";
   }
 
-  if (!config.ref){
-    throw 'Database ref Not Present';
-  }
-
-  if (!config.operation){
-    throw 'Write Operation is not defined';
-  }
-
-  this.ref = config.ref;
-  this.operation = config.operation;
-  this.database = config.admin.database;
 	this.onStatus = ()=>{}
 }
 
 FirebaseOutNode.prototype.onInput = function(msg, out, errorcb) {
-  const refPath = msg.ref || this.ref;
-  const operation = msg.operation || this.operation;
-
-  this.database.ref().child(refPath)[operation](msg.payload, error => {
-    if (error){
-      errorcb(error);
-    } else {
-      out(msg);
-    }
-  });
-
-  // let first = true
-  // ref.on('value', snapshot => {
-  //   // output on result, or immediately when there's no update
-  //   if (!first || (msg.payload === snapshot.val())){
-  //     out({payload: snapshot.val()})
-  //     return;
-  //   }
-  //   first = false;
-  //   if (!snapshot.exists()){
-  //     this.database.ref().child(refPath).set(msg.payload, error => {
-  //       if (error){
-  //         errorcb(error)
-  //       } 
-  //     });
-  //   } else {
-  //     ref.set(msg.payload, error => {
-  //       if (error){
-  //         errorcb(error)
-  //       } 
-  //     });
-  //   }
-  // });
+  // send msg, on ok call out, error call errorcb
+  out(msg);
 };
 
 FirebaseOutNode.prototype.setStatusCallback = function(cb) {
--- test/FirebaseOutNode_Spec.js
+++ test/FirebaseOutNode_Spec.js
@@ -2,75 +2,17 @@ const FirebaseAdminNode = require('../src/firebaseAdminNode');
 const assert = require('assert');
 
 const FirebaseOutNode = require('../src/firebaseOutNode');
-const serviceAccountJson = require('./my-firebase-service-credential.json');
+const serviceAccountJson = require('../my-firebase-service-credential.json');
 
 describe('FirebaseOutNode', function() {
-	it('Fail without ref', function(done) {
-  	const firebaseAdminNode = new FirebaseAdminNode({
-			serviceAccountJson: serviceAccountJson
-		});
-  	try {
-	  	const firebaseOutNode = new FirebaseOutNode({
-        operation: 'set',
-	  		admin: firebaseAdminNode
-	  	});
-	  	assert.fail()
-  	} catch (e){
-      firebaseAdminNode.onClose(null, done);
-  	}
-  });
-
-  it('Fails without operation', function(done) {
-    const firebaseAdminNode = new FirebaseAdminNode({
-      serviceAccountJson: serviceAccountJson
-    });
-    try {
-      const firebaseInNode = new FirebaseOutNode({
-        ref: 'test',
-        admin: firebaseAdminNode
-      });
-      assert.fail()
-    } catch (e){
-      firebaseAdminNode.onClose(null, done);
-    }
-  });
-
   it('Fail without admin', function() {
   	try {
-	  	const firebaseOutNode = new FirebaseOutNode({
-	  		ref: 'test',
-        operation: 'set'
-	  	});
+	  	new FirebaseOutNode({});
 	  	assert.fail()
   	} catch (e){}
   });
 
-  it('Can set data to firebase', function(done) {
-    this.timeout(3000);
-
-  	const firebaseAdminNode = new FirebaseAdminNode({
-			serviceAccountJson: serviceAccountJson
-		});
-
-  	const firebaseOutNode = new FirebaseOutNode({
-  		admin: firebaseAdminNode,
-      operation: 'set',
-  		ref: `${Math.floor(Math.random() * 100)}`
-  	});
-
-    const toBeUpdated = Math.floor(Math.random() * 100);
-
-  	firebaseOutNode.onInput({
-      payload: toBeUpdated
-    }, d => {
-      assert(d.payload === toBeUpdated);
-      firebaseAdminNode.onClose(null, done);
-    }, e => {
-      firebaseAdminNode.onClose(null, ()=>{done(1)});
-    });
-  });
-
-  it('Can push data to firebase', function(done) {
+  it('Can send cloud messaging', function(done) {
     this.timeout(3000);
 
     const firebaseAdminNode = new FirebaseAdminNode({
@@ -78,17 +20,18 @@ describe('FirebaseOutNode', function() {
     });
 
     const firebaseOutNode = new FirebaseOutNode({
-      admin: firebaseAdminNode,
-      operation: 'push',
-      ref: `${Math.floor(Math.random() * 100)}`
+      admin: firebaseAdminNode
     });
 
-    const toBeUpdated = Math.floor(Math.random() * 100);
+    const topic = "kioskId == 1"
+    const content = Math.floor(Math.random() * 100);
 
     firebaseOutNode.onInput({
-      payload: toBeUpdated
+      payload: content,
+      topic
     }, d => {
-      assert(d.payload === toBeUpdated);
+      assert(d.payload === content);
+      assert(d.topic === topic);
       firebaseAdminNode.onClose(null, done);
     }, e => {
       firebaseAdminNode.onClose(null, ()=>{done(1)});
